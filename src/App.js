eb59afec443813ecec15bdaab4b3e02f0cb55fba
Precious <ikpaprecious2@gmail.com>
2022-12-12 14:12:52 +0100

    shopping

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import "./App.css";
-import Auth from "./component/Auth";
-import Header from "./component/Header";
-import { UserContextProvider } from "./context/userContext";
 
 function App() {
   return (
-    <div className="ui container">
-      <UserContextProvider>
-        <Header />
-        <Auth />
-      </UserContextProvider>
+    <div className="">
+
     </div>
   );
 }
